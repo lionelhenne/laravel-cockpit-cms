f07426af5256c82bf95105d87048d8722d6e9da6
Lionel Henne <lionelhenne@gmail.com>
2026-01-01 18:03:23 +0100

    fix: use dynamic method call on $this and include trait

--- src/Console/Commands/WarmupCockpitImages.php
+++ src/Console/Commands/WarmupCockpitImages.php
@@ -5,9 +5,11 @@ namespace lionelhenne\LaravelCockpitCms\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use lionelhenne\LaravelCockpitCms\Facades\Cockpit;
+use App\Http\Controllers\Traits\CockpitGQLQueries;
 
 class WarmupCockpitImages extends Command
 {
+    use CockpitGQLQueries;
     protected $signature = 'cockpit:warmup {--queries= : Liste des requêtes GQL séparées par une virgule}';
     protected $description = 'Aspirer toutes les images de Cockpit vers le stockage local';
 
@@ -23,11 +25,16 @@ class WarmupCockpitImages extends Command
         foreach ($queryMethods as $method) {
             $this->line("Exécution de la requête : {$method}");
             
-            // On suppose que ces méthodes existent dans ton service ou trait
-            // Adapte ici selon comment tu récupères tes données dans le package
-            $result = Cockpit::execute([$method()]); 
+            // Correction ici : on appelle la méthode sur $this
+            if (!method_exists($this, $method)) {
+                $this->error("La méthode {$method} n'existe pas dans le trait.");
+                continue;
+            }
+
+            // On appelle la méthode du trait et on passe le résultat à Cockpit::execute
+            $gql = $this->{$method}(); 
+            $result = Cockpit::execute([$gql]); 
             
-            // Extraction récursive des chemins d'images (logique simplifiée)
             $paths = $this->extractImagePaths($result);
 
             foreach ($paths as $path) {
